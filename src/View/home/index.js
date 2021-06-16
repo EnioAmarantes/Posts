51ebc2cd1cfde9c0b19fda7900d36a8044329a88
EnioAmarantes <enio.amarantes@gmail.com>
2021-06-16 08:08:40 -0300

    Conexão com os posts do Firebase

--- src/View/home/index.js
+++ src/View/home/index.js
@@ -1,26 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./home.css";
 import {Link} from "react-router-dom";
 import { useSelector } from 'react-redux';
 
 import NavBar from "../../components/navbar";
 import PostCard from "../../components/postcard";
+import firebase from "../../config/firebase";
 
 function Home() {
+
+    const [posts, setPosts] = useState([]);
+    var listaPosts = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('posts').get().then(async (res) => {
+            await res.docs.forEach( doc => {
+                listaPosts.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            setPosts(listaPosts);
+        })
+    });
+
     return(
         <>
        <NavBar />
        {/* <h1>{useSelector(state => state.userEmail)}</h1> */}
-       <div className="row">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+       <div className="row p-2">
+           {
+               posts.map( item => 
+                <PostCard key={item.id} id={item.id} title={item.titulo} description={item.descricao} view={item.visualizacoes} imagem={item.image} />
+               )
+           }
        </div>
        </>
     );
--- src/components/postcard/index.js
+++ src/components/postcard/index.js
@@ -2,21 +2,32 @@ import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import "./postcard.css";
 
-function PostCard(){
+import firebase from "../../config/firebase";
+
+function PostCard({id, title, description, view, imagem}){
+
+    const [urlImagem, setUrlImagem] = useState();
+
+    useEffect( () => {
+        firebase.storage().ref(`imagens/${imagem}`).getDownloadURL().then( url => {
+            setUrlImagem(url);
+        })
+    })
+
     return(
         <div className="col-md-3 col-sm-12">
-            <img id="imgCard" src="https://via.placeholder.com/100x50" className="card-img-top" />
+            <img id="imgCard" src={urlImagem} className="card-img-top" />
 
             <div className="card-body">
-                <h5>Título do post</h5>
-                <p className="card-text text-justify">Detalhes do post</p>
+                <h5>{ title }</h5>
+                <p className="card-text text-justify">{description}</p>
 
                 <div className="row rodape-card d-flex align-items-center">
                     <div className="col-6">
-                        <Link to="/" className="btn btn-sm btn-detalhes">+ detalhes</Link>
+                        <Link to={`/postdetalhes/${id}`} className="btn btn-sm btn-detalhes">+ detalhes</Link>
                     </div>
                     <div className="col-6">
-                        <i class="fas fa-eye"></i><span>29305</span>
+                        <i class="fas fa-eye"></i><span>{view}</span>
                     </div>
                 </div>
             </div>
